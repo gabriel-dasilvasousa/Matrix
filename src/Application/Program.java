a876a62d454f6dd24b9733429e04eb27463c5ed9
Gabriel da Silva Sousa <g.silva13455@gmail.com>
2021-03-30 17:57:13 -0300

    More functions fot to handle with Matrices

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -8,15 +8,35 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Matrix matrix = new Matrix(2, 5);
-	    
+		Matrix matrix = new Matrix(3, 2);
+		Matrix matrix2 = new Matrix(2,3);
 		
-		matrix.setValue(1, 2, 3);
+		matrix.setValuesInMultipleLines(Arrays.asList(3,2), Arrays.asList(3,3), Arrays.asList(1,2));
+		matrix2.setValuesInMultipleLines(Arrays.asList(3,2,6), Arrays.asList(1,2,5));
 		
-	    matrix.setValuesPerLine(0, Arrays.asList(1,2,3,4,5));
+		Matrix result = Matrix.product(matrix, matrix2);
+		
+		
+		matrix.getValuesToInt();
+		System.out.println();
+		matrix2.getValuesToInt();
+		
+		System.out.println();
+		result.getValuesToInt();
+		//result.getValuesToInt();
+		
+		
+		//System.out.println(Matrix.verifyIfProductIsPossible(matrix, matrix2));
+		
+		//matrix.setValue(1, 2, 3);
+		
+	    /*matrix.setValuesPerLine(0, Arrays.asList(1,2,3,4,5));
 	    matrix.setValuesPerLine(1, Arrays.asList(3,4));
+	    */
+		
+	    //matrix.setValuesInMultipleLines();
 	    
-	    matrix.getValuesToInt();
+	    //matrix.getValuesToInt();
 
 	}
 
--- src/Util/Matrix.java
+++ src/Util/Matrix.java
@@ -14,7 +14,7 @@ public class Matrix{
 	    matrix = new int[lines][columns];     
 	  }
 
-	  public static boolean verifyProduct(Matrix m1, Matrix m2){
+	  public static boolean verifyIfProductIsPossible(Matrix m1, Matrix m2){
 	    if(m1.columns != m2.lines){
 	      return false;
 	    }
@@ -28,18 +28,66 @@ public class Matrix{
 	  public int getColumns(){
 	    return columns;
 	  }
-
+	  
+	  public int getValue(int line, int column) {
+		  return matrix[line][column];
+	  }
+	  
+	  public int[] getLine(int line) {
+		  if(line>getLines()) throw new IllegalArgumentException();
+		  int[] lineValues = new int[this.columns];
+		  for(int i=0; i<this.columns; i++) {
+			  lineValues[i] = matrix[line][i];
+		  }
+		  return lineValues;
+	  }
+	  
+	  public int[] getColumn(int column) {
+		  if(column>getColumns()) throw new IllegalArgumentException();
+		  int[] columnValues = new int[this.lines];
+		  for(int i=0; i<this.lines; i++) {
+			  columnValues[i] = matrix[i][column];
+		  }
+		  return columnValues;
+	  }
+	  
 	  public void setValue(int line, int column, int value){
 	    matrix[line][column] = value;
 	  }
 	  
 	  public void setValuesPerLine(int linha, List<Integer> elementos) {	
-		  if(elementos.size()>columns) {
-			  throw new IllegalArgumentException();
-		  }
+		  if(elementos.size()>columns) throw new IllegalArgumentException();
 		  for(int i=0; i<elementos.size(); i++) {
 			  matrix[linha][i] = elementos.get(i);
 		  }
+	  }
+	  
+	  public void setValuesInMultipleLines(List<Integer>... lines) {
+		  if(lines.length > this.lines) throw new IllegalArgumentException();
+		  for(int i=0; i<lines.length;i++) {
+			  setValuesPerLine(i, lines[i]);
+		  }
+	  }
+	  
+	  public static Matrix product(Matrix m1, Matrix m2) {
+		  if(!verifyIfProductIsPossible(m1, m2)) throw new IllegalArgumentException("Number of lines and columns incompatibles");
+		  
+		  Matrix matrixResult = new Matrix(m1.getLines(),m2.getColumns());
+		  
+		  for(int i=0; i<m1.getLines(); i++) {
+			  int value = 0;
+			  for(int j=0; j<m2.getColumns(); j++) {
+				  int valueInLine[] = m1.getLine(i);
+				  for(int k=0; k<m2.getLines(); k++) {
+					  int valInLine = valueInLine[k];
+					  int valueInColumn = m2.getColumn(j)[k];  
+					  value += valInLine*valueInColumn;
+				  }
+				  matrixResult.setValue(i, j, value);
+				  value=0;
+			  }
+		  }
+		  return matrixResult;
 	  }
 	  
 	  public void getValuesToInt(){
